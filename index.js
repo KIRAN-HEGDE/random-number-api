d76c51950e6e4b84e48012d6832b06abdd091c15
Kiran Hegde <kiranbanjagaric@gmail.com>
2021-03-03 18:26:41 +0530

    Updated package.json

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/random/:min/:max', (req, res) => {
+   let min = parseInt(req.params.min);
+   let max = parseInt(req.params.max);
+   if (isNaN(min) || isNaN(max)) {
+       res.status(400);
+       res.json({error: "Bad Request."});
+       return;
+   }
+
+   result = Math.round((Math.random() * (max-min) ) + min);
+   res.json({result: result});
+});
+
+app.listen(3000, () => {
+ console.log('App Started on port 3000');   
+});
